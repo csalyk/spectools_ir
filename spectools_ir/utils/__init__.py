63f5a6ff8000b5e64032341fbcc555ef88f70b65
Colette Salyk <cosalyk@vassar.edu>
2022-06-30 17:50:45 -0400

    Added extract_HITRAN_from_par routine to utils

--- spectools_ir/utils/__init__.py
+++ spectools_ir/utils/__init__.py
@@ -1,3 +1,4 @@
 from .utils import extract_hitran_data, compute_thermal_velocity, sigma_to_fwhm, spec_convol, make_rotation_diagram
 from .utils import fwhm_to_sigma, wn_to_k, get_global_identifier, translate_molecule_identifier, get_molecule_identifier
 from .utils import get_molmass, spec_convol_R, get_miri_mrs_resolution, get_miri_mrs_wavelengths, make_miri_mrs_figure
+from .utils import extract_HITRAN_from_par
